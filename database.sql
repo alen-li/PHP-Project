2210852f4ed4a1865c58dabcd11172727f5d5124
alen-li <seed0111@gmail.com>
2016-04-14 09:56:04 +0800

    Default codeigniter Project to New member.
    新人訓練用第三階段範本教材

---
+++ database.sql
@@ -0,0 +1,41 @@
+use mysql;
+create database task_backend default character set utf8 default collate utf8_general_ci;
+
+use task_backend;
+--account : email address
+--powerflag : 0 is normal, 1 is admin
+--delflag : 0 is active
+CREATE TABLE `work_account` (
+  `aid` VARCHAR( 50 ) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `account` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `powerflag` int(1) NOT NULL default 0,
+  `delflag` int(1) NOT NULL default 0,
+  `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TRIGGER t_work_account
+  BEFORE INSERT ON work_account
+  FOR EACH ROW
+  SET new.aid = uuid();
+  
+CREATE TABLE `work_system` (
+  `sid` VARCHAR( 50 ) NOT NULL,
+  `sysname` varchar(255) NOT NULL,
+  `controler` varchar(255) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TRIGGER t_work_system
+  BEFORE INSERT ON work_system
+  FOR EACH ROW
+  SET new.sid = uuid();
+  
+CREATE TABLE `work_permissions` (
+  `aid` VARCHAR( 50 ) NOT NULL,
+  `sid` VARCHAR( 50 ) NOT NULL,
+  PRIMARY KEY (`aid`,`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
