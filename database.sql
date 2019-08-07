732b24686ff00a7038da18ac94a86a30f4e5b80c
seed0111 <seed0111@gmail.com>
2019-08-07 11:57:10 +0800

    add test.txt

--- database.sql
+++ database.sql
@@ -2,16 +2,16 @@ use mysql;
 create database task_backend default character set utf8 default collate utf8_general_ci;
 
 use task_backend;
---account : email address
---powerflag : 0 is normal, 1 is admin
---delflag : 0 is active
+-- account : email address
+-- powerflag : 0 is normal, 1 is admin
+-- delflag : 0 is active
 CREATE TABLE `work_account` (
   `aid` VARCHAR( 50 ) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `account` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `powerflag` int(1) NOT NULL default 0,
-  `delflag` int(1) NOT NULL default 0,
+  `powerflag` int(1) NOT NULL default 0 COMMENT '授權旗標', 
+  `delflag` int(1) NOT NULL default 0 COMMENT '刪除旗標', 
   `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`aid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -23,8 +23,8 @@ CREATE TRIGGER t_work_account
   
 CREATE TABLE `work_system` (
   `sid` VARCHAR( 50 ) NOT NULL,
-  `sysname` varchar(255) NOT NULL,
-  `controler` varchar(255) NOT NULL,
+  `sysname` varchar(255) NOT NULL COMMENT '系統別', 
+  `controler` varchar(255) NOT NULL COMMENT '控制項識別', 
   PRIMARY KEY (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
